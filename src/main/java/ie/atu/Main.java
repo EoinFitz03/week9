2811c3a9b3c91c2799f60d400565bde3f1353ae0
EoinFirz03 <G00418325@atu.ie>
2023-12-04 10:17:55 +0000

    adding in super

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -2,6 +2,10 @@ package ie.atu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person myPerson = new Person("Eoin","cortoon","122445");
+        System.out.println(myPerson.toString());
+
+        Customer myCustomer = new Customer("Eoin","cortoon","1234","1234",true);
+        System.out.println(myCustomer.toString());
     }
 }
